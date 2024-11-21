c7c6f3845ac27ced830f53340bf1434b33e6e6ae
repowianata <repowianata@gmail.com>
2024-11-21 21:05:39 +0700

    “image”

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBCmmef1Eq0QC8XiD-pAaecWwTaqS9ihBA",
+    authDomain: "vue-firebase-7a8e0.firebaseapp.com",
+    projectId: "vue-firebase-7a8e0",
+    storageBucket: "vue-firebase-7a8e0.firebasestorage.app",
+    messagingSenderId: "785879359719",
+    appId: "1:785879359719:web:690515187a4fc5ca7d4242"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
